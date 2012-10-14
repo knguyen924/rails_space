6e1e67af7fae6f4544423f1975013ae9e0d50802
Khang N. Nguyen <khanguy3n@gmail.com>
2012-10-14 11:58:09 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSpace::Application.config.secret_token = 'd7c866285c4665c5bd633d6a6e71b097daac147cbdcf0029a6b905b2e89172301149cc9b814d3f6ce4622df062ca9f8301c5443c4ca04dde8b7a4eb1620d7179'
